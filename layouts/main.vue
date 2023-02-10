3db4eb9f652998a7d5800360d70a5c61e723e5d6
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2023-02-10 18:12:12 +0800

    add: layouts

---
+++ layouts/main.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app">
+    <header role="banner">
+      <nav>
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/">Discovery</NuxtLink>
+        <NuxtLink to="/">Settings</NuxtLink>
+      </nav>
+    </header>
+    <main role="main">
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style lang="scss">
+.app {
+  height: 100%;
+  display: flex;
+  min-height: 512px;
+  > header {
+    flex-grow: 1;
+    flex-shrink: 0;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    user-select: none;
+
+    > nav {
+      width: 275px;
+      display: flex;
+      flex-direction: column;
+    }
+  }
+  > main {
+    flex-basis: auto;
+    flex-grow: 1;
+    flex-shrink: 1;
+
+    display: flex;
+  }
+}
+</style>
---
+++ layouts/primary.vue
@@ -0,0 +1,35 @@
+<template>
+  <NuxtLayout name="main">
+    <ClientOnly>
+      <div class="primary">
+        <slot />
+      </div>
+      <aside class="sidebar">
+        <nav>
+          <span>Login</span>
+          <span>Register</span>
+        </nav>
+      </aside>
+    </ClientOnly>
+  </NuxtLayout>
+</template>
+
+<style lang="scss">
+.primary {
+  max-width: 600px;
+  width: 100%;
+  border-left: 1px solid #123;
+  border-right: 1px solid #123;
+}
+.sidebar {
+  width: 350px;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  nav {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
