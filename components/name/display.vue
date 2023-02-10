c6ab9a674b865388a01529c814f78a650d6b51cc
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2023-02-10 18:12:46 +0800

    add: name components

---
+++ components/name/display.vue
@@ -0,0 +1,26 @@
+<template>
+  <span class="name-display" :class="{ header }">
+    <slot>{{ value }}</slot>
+  </span>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  value?: string;
+  header?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: "",
+  header: false,
+});
+</script>
+
+<style lang="scss">
+.name-display {
+  font-weight: bold;
+  &.header {
+    font-size: 1.667rem;
+  }
+}
+</style>
---
+++ components/name/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <span>
+    <slot name="mention">@</slot>
+    <slot>{{ value }}</slot>
+  </span>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  value?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: "",
+});
+</script>
