7c5e547ce3ff3ac9fd71ded38eaaeeb541eb7522
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2023-02-10 18:12:26 +0800

    add: avatar.vue

---
+++ components/avatar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="avatar">
+    <img v-if="imageUrl" :src="imageUrl" />
+    <img v-if="!imageUrl" src="@/assets/images/undraw_cat_px71.svg" />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  imageUrl?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {});
+</script>
+
+<style lang="scss">
+.avatar {
+  background-color: rgba(16, 95, 10, 0.2);
+  border-radius: 16.7%;
+  min-width: 10px;
+  min-height: 10px;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
