fcc6829c81694a48f1f6e1769de8e5f1b374810e
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2023-02-10 18:13:33 +0800

    add: space & button components

---
+++ components/nostring/button/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <slot>{{ label }} </slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  label: "",
+});
+</script>
---
+++ components/nostring/space.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="space"
+    :class="{
+      vertical,
+    }"
+  >
+    <slot>{{ content }} </slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  content?: string;
+  vertical?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  vertial: false,
+  content: "",
+});
+</script>
+
+<style lang="scss" scope>
+.space {
+  display: flex;
+}
+.vertical {
+  flex-direction: column;
+}
+</style>
