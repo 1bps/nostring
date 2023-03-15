a1b8b5bad0481e408b9da29789ab9dc71646510a
Dallas Lu <914202+dallaslu@users.noreply.github.com>
2023-03-15 19:31:31 +0800

    update

---
+++ src/lib/ui/Space.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    export let content: string = "";
+    export let vertical: boolean;
+    export let inline: boolean;
+    export let gap: number;
+    export let justify: string;
+    export let align: string;
+</script>
+
+<div
+    class="ns-space"
+    class:ns-vertical={vertical}
+    class:ns-inline={inline}
+    style:gap={gap ? `${gap}px` : undefined}
+    style:justify-content={justify}
+    style:align-items={align}
+>
+    <slot>{{ content }}</slot>
+</div>
+
+<style lang="scss">
+    .ns-space {
+        display: flex;
+        gap: 5px;
+    }
+
+    .ns-vertical {
+        flex-direction: column;
+    }
+
+    .ns-inline {
+        display: inline-flex;
+    }
+</style>
---
+++ src/lib/ui/Text.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    export let content: string = "";
+    export let type: string = "default";
+    export let size: string = "m";
+    export let ellipsis: boolean = false;
+</script>
+
+<span
+    class:ns-text-ellipsis={ellipsis}
+    class={["ns-text", `ns-text-${type}`, `ns-text-${size}`].join(" ")}
+>
+    <slot>{{ content }}</slot>
+</span>
+
+<style lang="scss">
+    .ns-text {
+        color: var(--text-color);
+
+        &.ns-text-size-xl {
+            font-size: 133.3%;
+        }
+
+        &.ns-text-size-l {
+            font-size: 116.7%;
+        }
+
+        &.ns-text-size-s {
+            font-size: 83.3%;
+        }
+
+        &.ns-text-size-xs {
+            font-size: 66.7%;
+        }
+
+        &.ns-text-primary {
+            --text-color: var(--text-color-primary);
+        }
+
+        &.ns-text-tertiary {
+            --text-color: var(--text-color-tertiary);
+        }
+        &.ns-text-success {
+            --text-color: var(--text-color-success);
+        }
+
+        &.ns-text-ellipsis {
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+    }
+</style>
